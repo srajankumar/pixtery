fdb8922323b7aac8a7320c6408d164e6ac2f59e8
srajan <kumarsrajan02@gmail.com>
2024-08-06 10:38:45 +0530

    add: port

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const WebSocket = require("ws");
-const wss = new WebSocket.Server({ port: 8080 });
+const PORT = process.env.PORT || 8080;
+const wss = new WebSocket.Server({ port: PORT });
 
 let drawerSocket = null;
 let guesserSocket = null;
