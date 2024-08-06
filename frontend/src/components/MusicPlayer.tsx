28fac47e7934b1fa6d6cfafed86b5fb53ab796de
srajan <kumarsrajan02@gmail.com>
2024-08-06 16:06:07 +0530

    fix: music player

--- frontend/src/components/MusicPlayer.tsx
+++ frontend/src/components/MusicPlayer.tsx
@@ -1,20 +1,34 @@
 "use client";
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Volume2, VolumeX } from "lucide-react";
 
 const MusicPlayer: React.FC = () => {
-  const [isMuted, setIsMuted] = useState<boolean>(false);
+  const [isMuted, setIsMuted] = useState<boolean>(() => {
+    const savedState = localStorage.getItem("isMuted");
+    return savedState === "true";
+  });
   const audioRef = useRef<HTMLAudioElement>(null);
 
   const toggleMute = () => {
-    setIsMuted(!isMuted);
+    const newMuteState = !isMuted;
+    setIsMuted(newMuteState);
+    localStorage.setItem("isMuted", newMuteState.toString());
     if (audioRef.current) {
-      audioRef.current.muted = !isMuted;
+      audioRef.current.muted = newMuteState;
     }
   };
 
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.muted = isMuted;
+      audioRef.current
+        .play()
+        .catch((error) => console.error("Failed to play audio:", error));
+    }
+  }, [isMuted]);
+
   return (
-    <div className="fixed top-5 right-5 ">
+    <div className="fixed top-5 right-5">
       <audio ref={audioRef} loop autoPlay>
         <source src="/a-lonely-cherry-tree.mp3" type="audio/mpeg" />
         Your browser does not support the audio element.
